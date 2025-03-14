9e72ca80c41635082accf7c687fad320bdce71e8
Harshit <harshitrawat@outlook.com>
2025-03-14 23:53:12 +0530

    Added task role

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -1,22 +1,39 @@
 # ECS luster for running app on Fargate
 
 resource "aws_iam_policy" "task_execution_role_policy" {
-    name = "${local.prefix}-task-exec-role-policy"
-    path = "/"
-    description = "Allow ECS to retrieve images and add to logs"
-    policy = file(".templates/ecs/task-execution-role-policy.json")
+  name        = "${local.prefix}-task-exec-role-policy"
+  path        = "/"
+  description = "Allow ECS to retrieve images and add to logs"
+  policy      = file(".templates/ecs/task-execution-role-policy.json")
 }
 
 resource "aws_iam_role" "task_execution_role" {
-    name = "${local.prefix}-task-execution-roe"
-    assume_role_policy = file(".templates/ecs/task-assume-role-policy.json")
+  name               = "${local.prefix}-task-execution-roe"
+  assume_role_policy = file(".templates/ecs/task-assume-role-policy.json")
 }
 
 resource "aws_iam_role_policy_attachment" "task_execution_role" {
-  role = aws_iam_role.task_execution_role.name
+  role       = aws_iam_role.task_execution_role.name
   policy_arn = aws_iam_policy.task_execution_role_policy.arn
 }
 
-resource "aw_ecs_cluster" "main"{
-    name = "${local.prefix}-cluster"
+resource "aws_iam_policy" "task_ssm_policy" {
+  name        = "${local.prefix}-task-ssm-role-policy"
+  path        = "/"
+  description = "Allow ECS to retrieve images and add to logs"
+  policy      = file(".templates/ecs/task-ssm-policy.json")
+}
+
+resource "aws_iam_role" "app-task" {
+  name               = "${local.prefix}-app-task"
+  assume_role_policy = file(".templates/ecs/task-assume-role-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "task_execution_role" {
+  role       = aws_iam_role.app-task.name
+  policy_arn = aws_iam_policy.task_ssm_policy.arn
+}
+
+resource "aw_ecs_cluster" "main" {
+  name = "${local.prefix}-cluster"
 }
