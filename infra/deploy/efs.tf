11aa5194043bd34e8db025ae6f3d9179023b20ed
Harshit <harshitrawat@outlook.com>
2025-03-31 20:56:21 +0530

    Added efs and ecs security group

--- infra/deploy/efs.tf
+++ infra/deploy/efs.tf
@@ -3,6 +3,19 @@ resource "aws_efs_file_system" "media" {
 
     tags = {
       Name = "${local.prefix}-media"
+    } 
+}
+
+resource "aws_security_group" "efs" {
+    name = "${local.prefix}-efs"
+
+    ingress {
+        from_port = 2049
+        to_port = 2049
+        protocol = "tcp"
+
+        security_groups = [
+            aws_security_group.ecs_service.id
+        ]
     }
-  
 }
